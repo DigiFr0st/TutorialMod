83b0aa0ba4db2037d392ffdd2fe4472a200f3c71
DigiFr0st <digifrost.business@gmail.com>
2023-02-07 17:36:46 -0500

    Version: 1.19-0.0.2.0
    Added
    + ModBlocks.java for registering new blocks
    + Blocks:
      + Deepslate Zircon Ore
      + Endstone Zircon Ore
      + Netherrack Zircon Ore
      + Zircon Block
      + Zircon Ore
    + blockNotes with some general development notes for future use.

--- src/main/java/net/digifr0st/tutorialmod/TutorialMod.java
+++ src/main/java/net/digifr0st/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.digifr0st.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.digifr0st.tutorialmod.block.ModBlocks;
 import net.digifr0st.tutorialmod.item.ModItems;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -21,6 +22,7 @@ public class TutorialMod {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
